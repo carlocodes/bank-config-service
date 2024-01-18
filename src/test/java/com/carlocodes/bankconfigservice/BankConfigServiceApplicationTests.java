1e353e09831aa18a540af85385d630c6f6d2b6ac
carlocodes <Gian Uy>
2024-01-18 20:22:08 +0800

    intial commit

---
+++ src/test/java/com/carlocodes/bankconfigservice/BankConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.bankconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
